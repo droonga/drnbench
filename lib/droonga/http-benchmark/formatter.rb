f1695a5d4f2c8be722383e3e5ecee5cb6f9668e9
YUKI Hiroshi <shimoda@clear-code.com>
2013-10-07 18:15:30 +0900

    Output result of GradualRunner as a CSV

--- lib/droonga/http-benchmark/formatter.rb
+++ lib/droonga/http-benchmark/formatter.rb
@@ -16,6 +16,35 @@ module Droonga
           puts "  max:     #{result[:max_elapsed_time} sec"
           puts "  average: #{result[:average_elapsed_time]} sec"
         end
+
+        def output_gradual_results(results)
+          http_statuses = []
+          results.each do |n_clients, result|
+            http_statuses += result[:responses].keys
+          end
+          http_statuses.uniq!
+          http_statuses.sort!
+
+          puts "n_clients,total_n_requests,queries_per_second," +
+                 "#{http_statuses.join(",")}," +
+                 "min_elapsed_time,max_elapsed_time,average_elapsed_time"
+          results.each do |n_clients, result|
+            response_statuses = http_statuses.collect do |status|
+              if result[:responses].include?(status)
+                result[:responses][status]
+              else
+                0
+              end
+            end
+            puts "#{n_clients}," +
+                   "#{result[:total_n_requests]}," +
+                   "#{result[:queries_per_second]}," +
+                   "#{response_statuses.join(",")}," +
+                   "#{result[:min_elapsed_time]}," +
+                   "#{result[:max_elapsed_time]}," +
+                   "#{result[:average_elapsed_time]},"
+          end
+        end
       end
     end
   end
--- lib/droonga/http-benchmark/gradual-runner.rb
+++ lib/droonga/http-benchmark/gradual-runner.rb
@@ -11,10 +11,19 @@ module Droonga
         @start_n_clients = params[:start_n_clients]
         @end_n_clients = params[:end_n_clients]
         @step = params[:step]
-        @results = []
+      end
+
+      def run
+        run_benchmarks
+        Formatter.output_gradual_results(@results)
+      end
+
+      private
+      def run_benchmarks
+        @results = {}
         @start_n_clients.step(@end_n_clients, @step) do |n_clients|
           benchmark = Runner.new(params.merge(:n_clients => n_clients))
-          @results << benchmark.run
+          @results[n_clients] = benchmark.run
         end
       end
     end
