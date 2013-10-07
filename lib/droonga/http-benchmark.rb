99c29f3695bc197032af1f24134c995d6279ad73
YUKI Hiroshi <shimoda@clear-code.com>
2013-10-07 15:21:41 +0900

    Use "n_XXX" style naming instead of "XXX_count"

--- lib/droonga/http-benchmark.rb
+++ lib/droonga/http-benchmark.rb
@@ -4,11 +4,11 @@ class HttpBenchmark
   attr_reader :duration, :threads_count
 
   MIN_DURATION = 1.0
-  MAX_THREADS_COUNT = 16
+  MAX_N_THREADS = 16
 
   def initialize(params)
     @duration = [params[:duration], MIN_DURATION].max
-    @threads_count = [params[:threads_count], MAX_THREADS_COUNT].min
+    @n_threads = [params[:n_threads], MAX_N_THREADS].min
   end
 
   def run
