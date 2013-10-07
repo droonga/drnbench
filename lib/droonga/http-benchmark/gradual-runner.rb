6a09303d0a47d59a5c5e81c8748c866ebd739042
YUKI Hiroshi <shimoda@clear-code.com>
2013-10-07 18:30:51 +0900

    Store parameters to a local variable for other methods

--- lib/droonga/http-benchmark/gradual-runner.rb
+++ lib/droonga/http-benchmark/gradual-runner.rb
@@ -15,6 +15,7 @@ module Droonga
         @start_n_clients = params[:start_n_clients] || DEFAULT_START_N_CLIENTS
         @end_n_clients = params[:end_n_clients] || DEFAULT_END_N_CLIENTS
         @step = params[:step] || DEFAULT_STEP
+        @params = params
       end
 
       def run
@@ -26,7 +27,7 @@ module Droonga
       def run_benchmarks
         @results = {}
         @start_n_clients.step(@end_n_clients, @step) do |n_clients|
-          benchmark = Runner.new(params.merge(:n_clients => n_clients))
+          benchmark = Runner.new(@params.merge(:n_clients => n_clients))
           @results[n_clients] = benchmark.run
         end
       end
