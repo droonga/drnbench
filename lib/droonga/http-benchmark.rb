2c072e642b8f3ad88261b0d2106b52ff0ff603a0
YUKI Hiroshi <shimoda@clear-code.com>
2013-10-07 18:19:10 +0900

    Specify path to sub modules with relative path

--- lib/droonga/http-benchmark.rb
+++ lib/droonga/http-benchmark.rb
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-require "http-benchmark/runner"
-require "http-benchmark/gradual-runner"
-require "http-benchmark/formatter"
+require "./http-benchmark/runner"
+require "./http-benchmark/gradual-runner"
+require "./http-benchmark/formatter"
