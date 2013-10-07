f3ea7e9f8c4dc0ab0d1cf591bf19ac4379df17fd
YUKI Hiroshi <shimoda@clear-code.com>
2013-10-07 15:48:43 +0900

    Add logic to populate requests

--- lib/droonga/http-benchmark.rb
+++ lib/droonga/http-benchmark.rb
@@ -6,12 +6,52 @@ class HttpBenchmark
   MIN_DURATION = 1.0
   MAX_N_THREADS = 16
 
+  TOTAL_N_REQUESTS = 1000,
+
   def initialize(params)
     @duration = [params[:duration], MIN_DURATION].max
     @n_threads = [params[:n_threads], MAX_N_THREADS].min
+    @n_requests = params[:n_requests] || TOTAL_N_REQUESTS
+
+    if params[:request_pattern]
+      params[:request_pattern][:frequency] = 1
+      @request_patterns = [params[:request_pattern]]
+    else
+      @request_patterns = params[:request_patterns]
+    end
+    populate_requests
   end
 
   def run
     raise "not implemented"
+  end
+
+  private
+  def populate_requests
+    @requests = []
+    @current_request = 0
+
+    @request_patterns.each do |request_pattern|
+      populate_request_pattern(request_pattern)
+    end
+
+    @requests.shuffle!
+  end
+
+  def populate_request_pattern(request_pattern)
+    frequency = request_pattern[:frequency].to_f
+    n_requests = @n_requests * frequency
+
+    base_patterns = nil
+    if request_pattern[:pattern]
+      base_patterns = [request_pattern[:pattern]]
+    end
+      base_patterns = request_pattern[:patterns]
+    else
+    base_patterns = base_patterns.shuffle
+
+    0.upto(n_requests) do |count|
+      @requests << base_patterns[count % base_patterns.size]
+    end
   end
 end
