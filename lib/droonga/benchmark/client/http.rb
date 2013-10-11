bbb317346cf5d393afb3cf952c3daae62084a43d
YUKI Hiroshi <shimoda@clear-code.com>
2013-10-11 10:23:44 +0900

    Separate definition of client and result classes from runner.rb

---
+++ lib/droonga/benchmark/client/http.rb
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+require "thread"
+require "net/http"
+require "json"
+
+module Droonga
+  module Benchmark
+      class HttpClient
+        attr_reader :requests, :results, :wait
+
+        DEFAULT_PATH = "/"
+        DEFAULT_METHOD = "GET"
+
+        def initialize(params)
+          @requests = params[:requests]
+          @result = params[:result]
+          @wait = params[:wait]
+
+          @default_host = params[:host]
+          @default_port = params[:port]
+          @default_path = params[:path] || DEFAULT_PATH
+          @default_method = params[:method] || DEFAULT_METHOD
+        end
+
+        def run
+          @thread = Thread.new do
+            loop do
+              request = @requests.pop
+              request[:host] ||= @default_host
+              request[:port] ||= @default_port
+              request[:path] ||= @default_path
+              request[:method] ||= @default_method
+              request[:method] = request[:method].upcase
+
+              Net::HTTP.start(request[:host], request[:port]) do |http|
+                header = {
+                  "user-agent" => "Ruby/#{RUBY_VERSION} Droonga::Benchmark::Runner::HttpClient"
+                }
+                response = nil
+                start_time = Time.now
+                case request[:method]
+                when "GET"
+                  response = http.get(request[:path], header)
+                when "POST"
+                  body = request[:body]
+                  unless body.is_a?(String)
+                    body = JSON.generate(body)
+                  end
+                  response = http.post(request[:path], body, header)
+                end
+                @result << {
+                  :request => request,
+                  :status => response.code,
+                  :elapsed_time => Time.now - start_time,
+                }
+              end
+              sleep @wait
+            end
+          end
+          self
+        end
+
+        def stop
+          @thread.exit
+        end
+      end
+  end
+end
