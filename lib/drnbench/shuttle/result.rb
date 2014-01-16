4c222b64f5bb22d1de64b9e1dae0757fe8152fc6
YUKI Hiroshi <shimoda@clear-code.com>
2014-01-16 16:53:36 +0900

    Indent

--- lib/drnbench/shuttle/result.rb
+++ lib/drnbench/shuttle/result.rb
@@ -1,114 +1,114 @@
 # -*- coding: utf-8 -*-
 
 module Drnbench
-module Shuttle
-  class Result
-    attr_reader :n_clients, :duration, :statuses
+  module Shuttle
+    class Result
+      attr_reader :n_clients, :duration, :statuses
 
-    class << self
-      def keys
-        [
-          :n_clients,
-          :total_n_requests,
-          :queries_per_second,
-          :min_elapsed_time,
-          :max_elapsed_time,
-          :average_elapsed_time,
-        ]
+      class << self
+        def keys
+          [
+            :n_clients,
+            :total_n_requests,
+            :queries_per_second,
+            :min_elapsed_time,
+            :max_elapsed_time,
+            :average_elapsed_time,
+          ]
+        end
       end
-    end
 
-    def initialize(params)
-      @n_clients = params[:n_clients]
-      @duration = params[:duration]
+      def initialize(params)
+        @n_clients = params[:n_clients]
+        @duration = params[:duration]
 
-      @results = []
-      @total_elapsed_time = 0.0
-      @elapsed_times = []
-      @statuses = {}
-    end
-
-    def <<(result)
-      clear_cached_statistics
-
-      @results << result
-
-      @statuses[result[:status]] ||= 0
-      @statuses[result[:status]] += 1
-
-      @elapsed_times << result[:elapsed_time]
-      @total_elapsed_time += result[:elapsed_time]
-    end
-
-    def total_n_requests
-      @total_n_requests ||= @results.size
-    end
-
-    def queries_per_second
-      @queries_per_second ||= total_n_requests.to_f / @duration
-    end
-
-    def status_percentages
-      @status_percentages ||= prepare_status_percentages
-    end
-
-    def min_elapsed_time
-      @min_elapsed_time ||= @elapsed_times.min
-    end
-
-    def max_elapsed_time
-      @max_elapsed_time ||= @elapsed_times.min
-    end
-
-    def average_elapsed_time
-      @average_elapsed_time ||= @total_elapsed_time / @elapsed_times.size
-    end
-
-    def to_s
-      "Total requests: #{total_n_requests} " +
-        "(#{queries_per_second} queries per second)\n" +
-      "Status:\n" +
-      status_percentages.collect do |status, percentage|
-        "  #{status}: #{percentage} %"
-      end.join("\n") + "\n" +
-      "Elapsed time:\n" +
-      "  min:     #{min_elapsed_time} sec\n" +
-      "  max:     #{max_elapsed_time} sec\n" +
-      "  average: #{average_elapsed_time} sec"
-    end
-
-    def values
-      self.class.keys.collect do |column|
-        send(column)
+        @results = []
+        @total_elapsed_time = 0.0
+        @elapsed_times = []
+        @statuses = {}
       end
-    end
 
-    private
-    def clear_cached_statistics
-      @total_n_requests = nil
-      @queries_per_second = nil
-      @status_percentages = nil
-      @min_elapsed_time = nil
-      @max_elapsed_time = nil
-      @average_elapsed_time = nil
-    end
+      def <<(result)
+        clear_cached_statistics
 
-    def prepare_status_percentages
-      status_percentages = []
-      @statuses.each do |status, n_results|
-        percentage = n_results.to_f / total_n_requests * 100
-        status_percentages << {:percentage => percentage,
-                               :status => status}
+        @results << result
+
+        @statuses[result[:status]] ||= 0
+        @statuses[result[:status]] += 1
+
+        @elapsed_times << result[:elapsed_time]
+        @total_elapsed_time += result[:elapsed_time]
       end
-      status_percentages.sort! do |a, b|
-        (-1) * (a[:percentage] <=> b[:percentage])
+
+      def total_n_requests
+        @total_n_requests ||= @results.size
       end
-      status_percentages = {}
-      status_percentages.each do |status|
-        status_percentages[status[:status]] = status[:percentage]
+
+      def queries_per_second
+        @queries_per_second ||= total_n_requests.to_f / @duration
       end
-      status_percentages
+
+      def status_percentages
+        @status_percentages ||= prepare_status_percentages
+      end
+
+      def min_elapsed_time
+        @min_elapsed_time ||= @elapsed_times.min
+      end
+
+      def max_elapsed_time
+        @max_elapsed_time ||= @elapsed_times.min
+      end
+
+      def average_elapsed_time
+        @average_elapsed_time ||= @total_elapsed_time / @elapsed_times.size
+      end
+
+      def to_s
+        "Total requests: #{total_n_requests} " +
+          "(#{queries_per_second} queries per second)\n" +
+        "Status:\n" +
+        status_percentages.collect do |status, percentage|
+          "  #{status}: #{percentage} %"
+        end.join("\n") + "\n" +
+        "Elapsed time:\n" +
+        "  min:     #{min_elapsed_time} sec\n" +
+        "  max:     #{max_elapsed_time} sec\n" +
+        "  average: #{average_elapsed_time} sec"
+      end
+
+      def values
+        self.class.keys.collect do |column|
+          send(column)
+        end
+      end
+
+      private
+      def clear_cached_statistics
+        @total_n_requests = nil
+        @queries_per_second = nil
+        @status_percentages = nil
+        @min_elapsed_time = nil
+        @max_elapsed_time = nil
+        @average_elapsed_time = nil
+      end
+
+      def prepare_status_percentages
+        status_percentages = []
+        @statuses.each do |status, n_results|
+          percentage = n_results.to_f / total_n_requests * 100
+          status_percentages << {:percentage => percentage,
+                                 :status => status}
+        end
+        status_percentages.sort! do |a, b|
+          (-1) * (a[:percentage] <=> b[:percentage])
+        end
+        status_percentages = {}
+        status_percentages.each do |status|
+          status_percentages[status[:status]] = status[:percentage]
+        end
+        status_percentages
+      end
     end
   end
-end
 end
--- lib/drnbench/shuttle/runner.rb
+++ lib/drnbench/shuttle/runner.rb
@@ -5,110 +5,110 @@ require "drnbench/client/http-droonga"
 require "drnbench/shuttle/result"
 
 module Drnbench
-module Shuttle
-  class Runner
-    attr_reader :duration, :n_clients, :result
+  module Shuttle
+    class Runner
+      attr_reader :duration, :n_clients, :result
 
-    MIN_DURATION = 1
-    DEFAULT_DURATION = 10
-    MIN_WAIT = 0
-    DEFAULT_WAIT = 1
-    DEFAULT_N_CLIENTS = 1
-    TOTAL_N_REQUESTS = 1000
+      MIN_DURATION = 1
+      DEFAULT_DURATION = 10
+      MIN_WAIT = 0
+      DEFAULT_WAIT = 1
+      DEFAULT_N_CLIENTS = 1
+      TOTAL_N_REQUESTS = 1000
 
-    def initialize(params)
-      @duration = [params[:duration] || DEFAULT_DURATION, MIN_DURATION].max
-      @n_clients = params[:n_clients] || DEFAULT_N_CLIENTS
-      @n_requests = params[:n_requests] || TOTAL_N_REQUESTS
+      def initialize(params)
+        @duration = [params[:duration] || DEFAULT_DURATION, MIN_DURATION].max
+        @n_clients = params[:n_clients] || DEFAULT_N_CLIENTS
+        @n_requests = params[:n_requests] || TOTAL_N_REQUESTS
 
-      params[:wait] ||= DEFAULT_WAIT
-      params[:wait] = [params[:wait], MIN_WAIT].max
+        params[:wait] ||= DEFAULT_WAIT
+        params[:wait] = [params[:wait], MIN_WAIT].max
 
-      @params = params
+        @params = params
 
-      if params[:request_pattern]
-        params[:request_pattern][:frequency] = 1
-        @request_patterns = [params[:request_pattern]]
-      else
-        @request_patterns = params[:request_patterns]
-      end
-      populate_requests
-    end
-
-    def run
-      process_requests
-      @result
-    end
-
-    private
-    def process_requests
-      requests_queue = Queue.new
-      @result = Result.new(:n_clients => @n_clients,
-                           :duration => @duration)
-
-      client_params = @params.merge(:requests => requests_queue,
-                                    :result => @result)
-      @clients = @n_clients.times.collect do |index|
-        client = nil
-        case @params[:mode]
-        when :http
-          client = HttpClient.new(client_params)
-        when :http_droonga
-          client = HttpDroongaClient.new(client_params)
+        if params[:request_pattern]
+          params[:request_pattern][:frequency] = 1
+          @request_patterns = [params[:request_pattern]]
+        else
+          @request_patterns = params[:request_patterns]
         end
-        client.run
-        client
+        populate_requests
       end
 
-      start_time = Time.now
-      while Time.now - start_time < @duration
-        if requests_queue.empty?
-          @requests.each do |request|
-            requests_queue.push(request)
+      def run
+        process_requests
+        @result
+      end
+
+      private
+      def process_requests
+        requests_queue = Queue.new
+        @result = Result.new(:n_clients => @n_clients,
+                             :duration => @duration)
+
+        client_params = @params.merge(:requests => requests_queue,
+                                      :result => @result)
+        @clients = @n_clients.times.collect do |index|
+          client = nil
+          case @params[:mode]
+          when :http
+            client = HttpClient.new(client_params)
+          when :http_droonga
+            client = HttpDroongaClient.new(client_params)
+          end
+          client.run
+          client
+        end
+
+        start_time = Time.now
+        while Time.now - start_time < @duration
+          if requests_queue.empty?
+            @requests.each do |request|
+              requests_queue.push(request)
+            end
+          end
+          sleep 1
+        end
+
+        @clients.each do |client|
+          client.stop
+        end
+
+        @result
+      end
+
+      def populate_requests
+        @requests = []
+
+        if @request_patterns.is_a?(Array)
+          @request_patterns.each do |request_pattern|
+            populate_request_pattern(request_pattern)
+          end
+        else
+          @request_patterns.each do |key, request_pattern|
+            populate_request_pattern(request_pattern)
           end
         end
-        sleep 1
+
+        @requests.shuffle!
       end
 
-      @clients.each do |client|
-        client.stop
-      end
+      def populate_request_pattern(request_pattern)
+        frequency = request_pattern[:frequency].to_f
+        n_requests = @n_requests * frequency
 
-      @result
-    end
-
-    def populate_requests
-      @requests = []
-
-      if @request_patterns.is_a?(Array)
-        @request_patterns.each do |request_pattern|
-          populate_request_pattern(request_pattern)
+        base_patterns = nil
+        if request_pattern[:pattern]
+          base_patterns = [request_pattern[:pattern]]
+        else
+          base_patterns = request_pattern[:patterns]
         end
-      else
-        @request_patterns.each do |key, request_pattern|
-          populate_request_pattern(request_pattern)
+        base_patterns = base_patterns.shuffle
+
+        n_requests.round.times do |count|
+          @requests << base_patterns[count % base_patterns.size]
         end
-      end
-
-      @requests.shuffle!
-    end
-
-    def populate_request_pattern(request_pattern)
-      frequency = request_pattern[:frequency].to_f
-      n_requests = @n_requests * frequency
-
-      base_patterns = nil
-      if request_pattern[:pattern]
-        base_patterns = [request_pattern[:pattern]]
-      else
-        base_patterns = request_pattern[:patterns]
-      end
-      base_patterns = base_patterns.shuffle
-
-      n_requests.round.times do |count|
-        @requests << base_patterns[count % base_patterns.size]
       end
     end
   end
-end
 end
